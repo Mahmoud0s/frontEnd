235aef201a11df3491eb27e6ae073275baa03301
Mahmoud0s <devmaos@gmail.com>
2025-03-19 03:16:25 +0200

    Add files via upload

---
+++ public/js/public/js/search.js
@@ -0,0 +1,91 @@
+let btn = document.getElementsByClassName("section-list")[0];
+btn.addEventListener("click", () => {
+    if(checkUser())
+        location.href="/upload";
+}
+);
+
+let searchBox=document.getElementsByClassName("search-list")[0];
+let input = searchBox.querySelector("input");
+let updatePopUp = document.getElementById("update-user-popUp");
+let idInput=document.querySelector("input[name='ID']");
+
+
+searchBox.addEventListener("input", async () => {
+    let content=searchBox.querySelector("div.content");
+        // let res = await fetch(`/api/search/search?query=${input.value}`);
+       getData(content,`http://localhost:5000/api/search/search?query=${input.value}`);
+        // let data =await res.json();
+        // content.textContent="";
+        
+        // if (data.length) {
+        //     const keys = Object.keys(data[0]);
+        //     const table = document.createElement("table");
+    
+        //     const thead = table.createTHead();
+        //     const tbody = table.createTBody();
+        //     const headerRow = thead.insertRow();
+    
+        //     keys.forEach(key => {
+        //         const th = document.createElement("th");
+        //         th.textContent = key;
+        //         headerRow.appendChild(th);
+        //     });
+        //     const actionHeader = document.createElement("th");
+        //     actionHeader.textContent = "Actions";
+        //     headerRow.appendChild(actionHeader);
+    
+        //     let counter=0;
+
+        //     data.forEach((rowData) => {
+        //         const row = tbody.insertRow();
+        //         keys.forEach((key,ind,arr) => {
+        //             const cell = row.insertCell();
+        //             if(key=="barcode"){
+        //                 console.log(cell);
+        //                 let img=document.createElement("img");
+        //                 img.setAttribute("id",`barcode-${counter}`)
+        //                 cell.appendChild(img)                      
+        //                 counter=counter+1;
+        //             }
+        //             else{
+        //                 cell.textContent = rowData[key];
+        //             }
+        //         });
+        //         const actionCell = row.insertCell();
+        //         const viewButton = document.createElement("button");
+        //         viewButton.innerText = "view";
+        //         viewButton.classList.add("edit");
+        //         viewButton.addEventListener("click", async () => {
+        //             location.href=`http://localhost:${location.port}${rowData.file_path}`;
+        //             content.textContent=""
+                    
+        //         });
+    
+        //         actionCell.appendChild(viewButton);
+        //     });
+    
+        //     content.appendChild(table);
+
+        //     data.forEach((rowData,ind) => {
+        //         JsBarcode(`#barcode-${ind}`,rowData.barcode,
+        //         {
+        //             format: "CODE128",
+        //             lineColor: "#000",
+        //             width: 2,
+        //             height: 50,
+        //             displayValue:true, 
+        //             fontSize: 16,
+        //         });
+                
+        //     });
+
+        // }
+        // else{            
+        //     content.textContent=`${data.message}`
+        // }
+
+})
+
+document.addEventListener("DOMContentLoaded", getWS);
+
